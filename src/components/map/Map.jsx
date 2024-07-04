60d08bb786a7484050380caceb90ecea5ba93ab4
AbdRofiqMm <abdulrofiqmahmud@gmail.com>
2024-07-05 00:00:37 +0800

    update: leaflet map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,8 +1,9 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./map.scss";
 import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
 
-const position = [51.505, -0.09];
+const position = [52.4797, -1.90269];
 
 function Map({ items }) {
   return (
@@ -16,11 +17,9 @@ function Map({ items }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
     </MapContainer>
   );
 }
